62d1bf5f1134a93623c0027db5ce3b1194ea04b6
damercadoo <damercadoo@unal.edu.co>
2021-05-22 16:05:57 -0500

    add final version of avl tree

---
+++ AVLTree/src/com/codewithmosh/Main.java
@@ -0,0 +1,12 @@
+package com.codewithmosh;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        var tree=new AVLTree();
+        tree.insert(10);
+        tree.insert(30);
+        tree.insert(20);
+    }
+}

dfe93a0b5545916f8abf943d8db0a681c17e05c4
damercadoo <damercadoo@unal.edu.co>
2021-07-18 16:10:22 -0500

    queue

--- Java2Lesson1/src/com/codewithmosh/Main.java
+++ Java2Lesson1/src/com/codewithmosh/Main.java
@@ -3,17 +3,24 @@ package com.codewithmosh;
 import com.codewithmosh.collections.Customer;
 import com.codewithmosh.collections.EmailComparator;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Customer> customers=new ArrayList<>();
-        customers.add(new Customer("b", "e3"));
-        customers.add(new Customer("a", "e2"));
-        customers.add(new Customer("c", "e1"));
-        Collections.sort(customers,new EmailComparator());
-        System.out.println(customers);
+        Queue<String>queue=new ArrayDeque<>();
+        queue.add("c");
+        queue.add("a");
+        queue.add("b");
+        queue.offer("d");
+        System.out.println(queue);
+        var front=queue.peek();//null if queue is empty
+        var front2=queue.element();//throw exception if queue is empty
+        System.out.println(front);
+        System.out.println(front2);
+        queue.remove();
+        System.out.println(queue);
+        System.out.println(queue.peek());
+        queue.poll();
+        System.out.println(queue);
 }}

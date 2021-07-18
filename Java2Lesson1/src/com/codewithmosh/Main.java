213a8e03ff63296939dd7c27968c3b61431213dc
damercadoo <damercadoo@unal.edu.co>
2021-07-18 15:30:10 -0500

    collections

--- Java2Lesson1/src/com/codewithmosh/Main.java
+++ Java2Lesson1/src/com/codewithmosh/Main.java
@@ -2,6 +2,7 @@ package com.codewithmosh;
 
 import com.codewithmosh.TextBox;
 import com.codewithmosh.UIControl;
+import com.codewithmosh.collections.CollectionsDemo;
 import com.codewithmosh.exceptions.Account;
 import com.codewithmosh.exceptions.AccountException;
 import com.codewithmosh.exceptions.ExceptionsDemo;
@@ -13,11 +14,5 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-      var list=new GenericList<String>();
-      list.add("a");
-      list.add("b");
-      for(var item:list)
-          System.out.println(item);
-    }
-
-}
+      CollectionsDemo.show();
+}}
--- Java2Lesson1/src/com/codewithmosh/collections/CollectionsDemo.java
+++ Java2Lesson1/src/com/codewithmosh/collections/CollectionsDemo.java
@@ -1,7 +1,22 @@
 package com.codewithmosh.collections;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class CollectionsDemo {
     public static void show(){
-        
+        Collection<String> collection=new ArrayList<>();
+        Collections.addAll(collection,"a","b","c");
+        collection.remove("a");
+        System.out.println(collection.contains("b"));
+        var objectArray=collection.toArray();
+        var stringArray=collection.toArray(new String[0]);
+        System.out.println(collection);
+        System.out.println(collection.size());
+        Collection<String> other=new ArrayList<>();
+        other.addAll(collection);
+        System.out.println(collection==other);
+        System.out.println(collection.equals(other));
     }
 }

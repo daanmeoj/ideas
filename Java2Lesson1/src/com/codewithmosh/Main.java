c835cd96651f3e60fc9a2b1b62837ec3f314849e
damercadoo <damercadoo@unal.edu.co>
2021-07-18 15:50:38 -0500

    comparable

--- Java2Lesson1/src/com/codewithmosh/Main.java
+++ Java2Lesson1/src/com/codewithmosh/Main.java
@@ -3,6 +3,7 @@ package com.codewithmosh;
 import com.codewithmosh.TextBox;
 import com.codewithmosh.UIControl;
 import com.codewithmosh.collections.CollectionsDemo;
+import com.codewithmosh.collections.Customer;
 import com.codewithmosh.exceptions.Account;
 import com.codewithmosh.exceptions.AccountException;
 import com.codewithmosh.exceptions.ExceptionsDemo;
@@ -10,9 +11,17 @@ import com.codewithmosh.exceptions.InsufficientFundsException;
 import com.codewithmosh.generics.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-      CollectionsDemo.show();
+        List<Customer> customers=new ArrayList<>();
+        customers.add(new Customer("b"));
+        customers.add(new Customer("a"));
+        customers.add(new Customer("c"));
+        Collections.sort(customers);
+        System.out.println(customers);
 }}
